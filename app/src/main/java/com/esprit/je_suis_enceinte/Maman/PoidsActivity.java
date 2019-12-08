0bf99a50b704226fcaf5aeddd668bd4498890a49
ramziarraki <ramzi.arraki@esprit.tn>
2019-12-08 20:16:25 +0100

    profile+package Maman(Poids ,ventre)

---
+++ app/src/main/java/com/esprit/je_suis_enceinte/Maman/PoidsActivity.java
@@ -0,0 +1,14 @@
+package com.esprit.je_suis_enceinte.Maman;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PoidsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_poids);
+    }
+}
